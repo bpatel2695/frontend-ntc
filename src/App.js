2b7a58fbd254ddc7abca94a2e1d3957a59987cad
bpatel2695 <bpatel2695@gmail.com>
2024-12-29 00:28:53 -0500

    new functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import Header from "./Header"
-import Main from "./Main"
-import Footer from "./Footer"
-import Cart from './Cart'
-import Login from './Login'
-import Signup from './Signup'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { createContext, useContext, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import Cart from "./Cart";
+import Login from "./Login";
+import Signup from "./Signup";
+import Admin from "./Admin";
+export const UserContext = createContext();
+
 export default function App() {
+  const [user, setUser] = useState(0);
+  const [cart, setCart] = useState({});
+  const [products, setProducts] = useState([]);
   return (
     <div>
-        <BrowserRouter>
-        <Header name="John" age={25} />
-        <Routes>
-          <Route index element={<Main />} />
-          <Route path="main" element={<Main />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-        <Footer />
-        </BrowserRouter>
+      <BrowserRouter>
+        <UserContext.Provider value={{ user, setUser, cart, setCart, products, setProducts }}>
+          <>
+            <Header name="John" age={25} />
+            <Routes>
+              <Route index element={<Main />} />
+              <Route path="main" element={<Main />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="admin" element={<Admin />} />
+            </Routes>
+            <Footer />
+          </>
+        </UserContext.Provider>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-
--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 import "./Main.css";
+import axios from "axios";
+import { useContext } from "react";
+import { UserContext } from "./App";
 export default function Main() {
-    const products = [
-        {name: "Product 1", price: 34  },
-        {name: "Product 2", price: 74  },
-        {name: "Product 3", price: 50  },
-    ];
-  return ( 
+  const [qty, setQty] = useState(1);
+  const { user, setUser, cart, setCart, products, setProducts } =
+    useContext(UserContext);
+  const url = "http://localhost:8080/";
+  const showProducts = async () => {
+    const response = await axios.get(url);
+    setProducts(response.data);
+  };
+  useEffect(() => {
+    showProducts();
+  }, []);
+
+  const handleAdd = (id, qty) => {
+    
+    setCart((prev) => ({ ...prev, [id]: qty }));
+    setQty(1)
+    console.log(cart);
+  };
+
+  // const products = [
+  //   { name: "Product 1", price: 34 },
+  //   { name: "Product 2", price: 74 },
+  //   { name: "Product 3", price: 50 },
+  // ];
+
+  return (
     <div className="row">
-            {products.map((value, index) => (
-        <div className="box">
+      {products &&
+        products.map((value, index) => (
+          <div key={index} className="box">
             <h3>{value.name}</h3>
             <h4>${value.price}</h4>
-            <button>Add to Cart</button>
-        </div>
-    ))}
+            <select onChange={(e) => setQty(Number(e.target.value))}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            <button onClick={() => handleAdd(value._id, qty)}>
+              Add to Cart 
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
- 
